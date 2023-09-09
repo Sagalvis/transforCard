fe8148889acbe858942a906d951767bf1f02273e
Johan Díaz <jdiaz@ulearning.com.co>
2023-09-09 15:26:24 -0500

    act

--- Frontend/src/Components/admin/pages/archive/styledFooter.jsx
+++ Frontend/src/Components/admin/pages/archive/styledFooter.jsx
@@ -2,29 +2,28 @@ import styled from 'styled-components';
 
 export const ContainFooter = styled.div`
   display: flex;
-  background-color: red; 
+  /* background-color: red;  */
   width: 100%;
-  padding: 24px;
+  padding: 0 24px;
   box-sizing: border-box;
-  height: 20%;
+  height: 7.9%;
 `;
 
 export const Copyright = styled.div`
   width: 50%;
-  height: 20%;
-  background-color: salmon;
+  display: flex;
+  align-items: center;
+  /* background-color: salmon; */
 `;
 
 export const Tag_P_Copy = styled.p`
-  display: flex;
-  align-items: center;
-  justify-content: auto;
   margin: 0;
 `;
 
 export const FootText = styled.div`
-  width: 50%;
-  background-color: yellow;
+  /* background-color: yellow; */
   display: flex;
+  align-items: center;
   justify-content: flex-end;
+  width: 50%;
 `;
