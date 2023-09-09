d085f09e8e905e6e02ed468058590377463b3186
johandev99 <jdiaz@ulearning.com.co>
2023-09-09 12:04:39 -0500

    act

--- Frontend/src/Components/admin/pages/archive/styledFooter.jsx
+++ Frontend/src/Components/admin/pages/archive/styledFooter.jsx
@@ -1,13 +1,15 @@
 import styled from 'styled-components';
 
 export const ContainFooter = styled.div`
-  /* background-color: red;  */
+  background-color: red; 
   width: 100%;
   height: 20%;
+  display: flex;
 `;
 
 export const Copyright = styled.div`
-  
+  width: 50%;
+  background-color: salmon; 
 `;
 
 export const Tag_P_Copy = styled.p`
@@ -15,5 +17,6 @@ export const Tag_P_Copy = styled.p`
 `;
 
 export const FootText = styled.div`
-  
+  width: 50%;
+  background-color: yellow; 
 `;
