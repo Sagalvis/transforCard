01f955b6f94a241a8052dc1161cd48d95be88306
Sergio Galvis <chefosergio@gmail.com>
2023-09-08 21:49:22 -0500

    haciendo sidebar

---
+++ Frontend/src/Components/admin/sidebar/archive/arrayNavbar.jsx
@@ -0,0 +1,47 @@
+export const ButtonNarbar1 = [
+  {
+    id: 1,
+    title:'Home',
+    icon:"home",
+    to: ""
+  },
+  {
+    id:2 ,
+    title : 'Clientes',
+    icon: "clientes",
+    to:"",
+  },
+  {
+    id:3,
+    title:"Vehiculos",
+    icon:"vehiculo",
+    to:""
+  },
+  {
+    id:4,
+    title:"Vehiculos",
+    icon:"vehiculo",
+    to:""
+  },
+  {
+    id:5,
+    title:"Vehiculos",
+    icon:"vehiculo",
+    to:""
+  },
+  {
+    id:6,
+    title:"Vehiculos",
+    icon:"vehiculo",
+    to:""
+  },
+  {
+    id:7,
+    title:"Vehiculos",
+    icon:"vehiculo",
+    to:""
+  },
+
+];
+
+
