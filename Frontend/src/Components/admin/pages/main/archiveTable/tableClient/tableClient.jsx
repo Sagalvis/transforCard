f5bd9e67ff4c226496eb57267495087c3828d1e0
Johan Díaz <jdiaz@ulearning.com.co>
2023-09-11 20:41:46 -0500

    Se pasan controladores de pages, se pasa el buscador y se pasa el manejo de paginas, del componente main  el componente tableClient

--- Frontend/src/Components/admin/pages/main/archiveTable/tableClient/tableClient.jsx
+++ Frontend/src/Components/admin/pages/main/archiveTable/tableClient/tableClient.jsx
@@ -1,29 +1,32 @@
 /* eslint-disable react/prop-types */
 import { useEffect, useState } from "react";
-import {
-  ButtonOptions,
-  Buttons,
-  ContainTable,
-  Table,
-  Tbody,
-  Td,
-  Th,
-  Thead,
-  Tr,
-} from "./styledTableClient";
+import {ButtonHandle, ButtonOptions, Buttons, ContainCheck, ContainControls, ContainHandlePage, ContainMaxData, ContainSearch,ContainTable, ContainTextHandle, ControlHandle, Input, Label, Li, Option, Select, Table, Tag_P_Handle, Tbody, Td, Th, Thead, Tr, Ul, } from "./styledTableClient";
 import axios from "axios";
 import Modals from "../../../archive/modals";
 import { ContainInfoModal } from "../../../header/styledHeader";
 import TableVehicle from "../tableVehicle/tableVehicle";
-import FormVehicle, { BtnRegister, ButtonRegister } from "../../../header/archiveInputs/formVehicle";
+import FormVehicle, {
+  BtnRegister,
+  ButtonRegister,
+} from "../../../header/archiveInputs/formVehicle";
 
-const TableClient = ({ editUser, createVehicle, deleteUser}) => {
+const TableClient = ({ editUser, createVehicle, deleteUser }) => {
   /* Variable de estado para traer clientes */
   const [customer, setCustomer] = useState([]);
 
-  // Variable de estado para abrir y cerrar modal de tabla vehiculo 
+  // Variable de estado para abrir y cerrar modal de tabla vehiculo
   const [handleCloseVehicle, setHandleCloseVehicle] = useState(false);
   const [handleOpenFormVehicle, setHandleOpenFormVehicle] = useState(false);
+
+  // Variable de estado para filtrar busqueda
+  const [search, setSearch] = useState('');
+
+  const searching = (e) => {
+    setSearch(e.target.value);
+    console.log(e.target.value);
+  };
+
+  //Metodo de filtrado (en espera...)
 
   const getCustomer = async () => {
     try {
@@ -37,15 +40,15 @@ const TableClient = ({ editUser, createVehicle, deleteUser}) => {
   // Funcion para eliminar cliente de la tabla
   const deleteClient = async (item) => {
     try {
-    const result = await axios.delete(`http://localhost:3005/deletecustomer/${item.identificacion}`
-    );
-    console.log(result);
-    window.location.reload();
+      const result = await axios.delete(
+        `http://localhost:3005/deletecustomer/${item.identificacion}`
+      );
+      console.log(result);
+      window.location.reload();
     } catch (error) {
       console.log(error);
     }
   };
-
 
   useEffect(() => {
     getCustomer();
@@ -53,6 +56,33 @@ const TableClient = ({ editUser, createVehicle, deleteUser}) => {
 
   return (
     <>
+      <ContainCheck>
+        <Label type="checkbox">Empresas</Label>
+        <Input type="checkbox" />
+        <Label type="checkbox">Personas</Label>
+      </ContainCheck>
+
+      {/* Controladores */}
+
+      <ContainControls>
+        {/* Control "CANTIDAD DE REGISTROS" */}
+        <ContainMaxData>
+          <Label type="select">Cantidad de registros</Label>
+          <Select>
+            <Option value="option1">10</Option>
+            <Option value="option2">25</Option>
+            <Option value="option3">50</Option>
+            <Option value="option4">100</Option>
+          </Select>
+        </ContainMaxData>
+
+        {/* BUSCADOR */}
+        <ContainSearch>
+          <Label className="search">Buscar: </Label>
+          <Input value={search} onChange={searching} type="text" title="Buscar cliente"></Input>
+        </ContainSearch>
+      </ContainControls>
+
       {/* Contenedor de tabla */}
 
       <ContainTable>
@@ -82,10 +112,16 @@ const TableClient = ({ editUser, createVehicle, deleteUser}) => {
                     <Buttons title="Editar cliente">
                       <i className={editUser}></i>
                     </Buttons>
-                    <Buttons onClick={() => setHandleCloseVehicle(!handleCloseVehicle)} title="Vehículos">
+                    <Buttons
+                      onClick={() => setHandleCloseVehicle(!handleCloseVehicle)}
+                      title="Vehículos"
+                    >
                       <i className={createVehicle}></i>
                     </Buttons>
-                    <Buttons onClick={() => deleteClient(item)} title="Eliminar cliente">
+                    <Buttons
+                      onClick={() => deleteClient(item)}
+                      title="Eliminar cliente"
+                    >
                       <i className={deleteUser}></i>
                     </Buttons>
                   </ButtonOptions>
@@ -96,31 +132,66 @@ const TableClient = ({ editUser, createVehicle, deleteUser}) => {
         </Table>
       </ContainTable>
 
+      {/* Contenedor manejo de paginas */}
+
+      <ContainHandlePage>
+        <ContainTextHandle>
+          <Tag_P_Handle>
+            Mostrando registros del 1 al 2 de un total de 2 registros
+          </Tag_P_Handle>
+        </ContainTextHandle>
+
+        {/* Manejo de paginas */}
+        <ControlHandle>
+          <Ul>
+            <Li>
+              <ButtonHandle>Anterior</ButtonHandle>
+            </Li>
+            <Li title="Pagina actual" className="button-li">
+              <a href="/">1</a>
+            </Li>
+            <Li>
+              <ButtonHandle>Siguiente</ButtonHandle>
+            </Li>
+          </Ul>
+        </ControlHandle>
+      </ContainHandlePage>
+
       <Modals
-      status={handleCloseVehicle}
-      changeStatus={setHandleCloseVehicle}
-      titleModal={'Tus vehículos'}
-      changePosition={"start"}
+        status={handleCloseVehicle}
+        changeStatus={setHandleCloseVehicle}
+        titleModal={"Tus vehículos"}
+        changePosition={"start"}
       >
         <ContainInfoModal>
           {/* Tabla de vehiculos registrados */}
-          <TableVehicle 
+          <TableVehicle
             editVehicleTable={"fa-solid fa-pen-to-square"}
             deleteVehicleTable={"fa-solid fa-trash-can"}
           />
           <ButtonRegister>
-            <BtnRegister className="color-red" onClick={() => setHandleCloseVehicle(!handleCloseVehicle)}>Cancelar</BtnRegister>
-            <BtnRegister onClick={() => setHandleOpenFormVehicle(!handleOpenFormVehicle)}>Crear vehículo</BtnRegister>
+            <BtnRegister
+              className="color-red"
+              onClick={() => setHandleCloseVehicle(!handleCloseVehicle)}
+            >
+              Cancelar
+            </BtnRegister>
+
+            <BtnRegister
+              onClick={() => setHandleOpenFormVehicle(!handleOpenFormVehicle)}
+            >
+              Crear vehículo
+            </BtnRegister>
           </ButtonRegister>
         </ContainInfoModal>
       </Modals>
 
       {/* Ventana modal para el formulario de vehículos */}
       <Modals
-      status={handleOpenFormVehicle}
-      changeStatus={setHandleOpenFormVehicle}
-      titleModal={'Crear vehículo nuevo'}
-      changePosition={"start"}
+        status={handleOpenFormVehicle}
+        changeStatus={setHandleOpenFormVehicle}
+        titleModal={"Crear vehículo nuevo"}
+        changePosition={"start"}
       >
         <ContainInfoModal>
           {/* Formaulario para registro de vehículos */}
