d4e472a2cb5bd880b2c19c11f2528badca35ae51
Sergio Galvis <Sgalvis@ulearning.com.co>
2023-09-22 09:34:31 -0500

    act

--- Frontend/src/Components/index.jsx
+++ Frontend/src/Components/index.jsx
@@ -3,15 +3,14 @@ import { ContainerMain } from "./styles/styledIndex";
 import Dashboard from "./admin/index.admin";
 import Login from "./login/index.login";
 import NotFount from "./admin/pages/notFount";
-import { LOGIN, NOTFOUND, PRIVATE } from "../config/routes/patch.routes";
 
 const IndexRoutes = () => {
   return ( 
     <ContainerMain>
       <Routes>
-        <Route path={LOGIN} element={<Login/>}/>
-        <Route path={PRIVATE} element={<Dashboard/>}/>
-        <Route path={NOTFOUND} element={<NotFount/>}/>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/admin/*" element={<Dashboard/>}/>
+        <Route path="*" element={<NotFount/>}/>
       </Routes>
     </ContainerMain>
   );
