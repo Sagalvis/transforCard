4b45c109298faf5f58e90699010c94666a7320ba
Roy polo <roypolocarrillo@gmail.com>
2023-09-22 09:36:18 -0500

    trabajando en subir imagenes

--- Backend/src/routes/img.routes.js
+++ Backend/src/routes/img.routes.js
@@ -1,11 +1,14 @@
 import {Router} from "express";
-
+import multer from "multer";
+import {fileUpload} from "../controllers/imagen.controllers.js"
 import * as controllpost from "../controllers/imagen.controllers.js";
 
 const imgRouter = Router();
-
+const uploader = multer({fileUpload})
 /* Rutas para subir imagenes */
 
-imgRouter.post('/imagen', controllpost.imagenes);
+imgRouter.post('/imagen', uploader,(req, res)=>{
+    console.log(req.file)
+} /* ,controllpost.postImg */);
 
 export default imgRouter;
