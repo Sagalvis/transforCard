544ecc7bf4840f3bcb796cebbb60e4e30cfb7c15
Roy polo <roypolocarrillo@gmail.com>
2023-10-06 15:57:15 -0500

    act

--- Frontend/src/Components/index.jsx
+++ Frontend/src/Components/index.jsx
@@ -3,7 +3,6 @@ import { ContainerMain } from "./styles/styledIndex";
 import Login from "./login/index.login";
 import NotFound from "./notFound/NotFound";
 import { lazy, Suspense } from "react";
-import CheckClient from "./customer/checkVehicle/checkClient";
 
 // Carga el componente de Dashboard de manera dinámica (asíncrona)
 const Dashboard = lazy(() => import("./admin/index.admin"));
@@ -21,7 +20,7 @@ const IndexRoutes = () => {
           <Route
             path="/admin/*"
             element={
-              <Suspense fallback={<h1>Cargando...</h1>}>
+              <Suspense fallback={<h1>Loading...</h1>}>
                 <Dashboard />
               </Suspense>
             }
@@ -29,11 +28,6 @@ const IndexRoutes = () => {
         ) : (
           <Route path="/admin/*" element={<Navigate to="/not-found" />} />
         )}
-
-        {/* Just routes for customers  */}
-        <Route path="/check" element={<CheckClient />} />
-        {/* ---------------->>> */}
-
         <Route path="/not-found" element={<NotFound />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
